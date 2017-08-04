e78d9931d2c4ddb7e5860e9f84b875dd8ed6abad
router <router@localhost.localdomain>
2017-08-03 22:42:24 -0300

    agregar concatenar js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,29 @@
 module.exports=function(grunt)
 {
-grunt.registerTask('minificarcss',function(){
-console.log("okey");
+//ELIMINAR CARPETAS DIST
+grunt.loadNpmTasks('grunt-contrib-clean');
+//CONCATENA VARIOS ARCHIVOS
+grunt.loadNpmTasks('grunt-contrib-concat');
+//MINIFICA JAVASCRIPT
+grunt.loadNpmTasks('grunt-contrib-uglify');
+//INICIO DE GRUNT
+grunt.initConfig({
+clean: ['dist/*'],
+concat:{
+	options:{
+		separator:';',
+	},
+	dist:
+	{
+	src:['js/*.js'],
+	dest:'dist/js/build.js'
+	}
+
+}
 });
 
-grunt.registerTask('default',['minificarcss']);
+
+
+grunt.registerTask('default',['clean','concat']);
 
 };
